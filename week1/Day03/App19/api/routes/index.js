c72492b778663a814bc195e7aedd329cb5373ac4
Adanetx <adane1301992@gamil.com>
2021-09-30 22:50:35 -0500

    path name change

--- week1/Day03/App19/api/routes/index.js
+++ week1/Day03/App19/api/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const calculateController = require("../controller/calculate")
-router.route("/calculate/:Id").get(calculateController.calculate)
+router.route("/calculate").get(calculateController.calculate)
 
 module.exports = router;
