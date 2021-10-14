cd2563503fdaa1bd0d77cfcb8eac0ad35e11afa9
Adanetx <adane1301992@gamil.com>
2021-10-13 23:05:59 -0500

    get one updated

--- week3/Day3/App01/mean-games/src/app/game-one/game-one.component.ts
+++ week3/Day3/App01/mean-games/src/app/game-one/game-one.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Game} from '../games-list/games-list.component';
+import { ActivatedRoute } from '@angular/router';
+
+import { GamesDataService } from '../games-data.service';
 @Component({
   selector: 'app-game-one',
   templateUrl: './game-one.component.html',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameOneComponent implements OnInit {
 
-  constructor() { }
+  game!: Game
+  constructor(private gamesDataService: GamesDataService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    let gameId = this.route.snapshot.params.gameId
+    console.log("gameId: ", gameId);
+    
+    this.gamesDataService.getGame(gameId).then(response => this.game = response)
+
+    this.gamesDataService.getGame(gameId).then(function(response){
+     // this.game = response;
+      console.log(response);
+      
+    })
+  }
   }
 
-}
+
